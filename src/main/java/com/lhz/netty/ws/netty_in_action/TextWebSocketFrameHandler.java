cafffe14b160a5b0d2acb4f5b5b421a4b8d01e9e
lhz <2190732276@qq.com>
2021-07-08 17:40:26 +0800

    netty in action 实例

---
+++ src/main/java/com/lhz/netty/ws/netty_in_action/TextWebSocketFrameHandler.java
@@ -0,0 +1,47 @@
+package com.lhz.netty.ws.netty_in_action;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+/**
+ * Date: 2021/7/8
+ * Description:
+ *
+ * @author hz.lai
+ */
+public class TextWebSocketFrameHandler
+        extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx,
+                                   Object evt) throws Exception {
+        if (evt instanceof   WebSocketServerProtocolHandler.HandshakeComplete) {
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            group.writeAndFlush(new TextWebSocketFrame(
+                    "Client " + ctx.channel() + " joined"));
+            group.add(ctx.channel());
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx,
+                             TextWebSocketFrame msg) throws Exception {
+        group.writeAndFlush(msg.retain());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        group.writeAndFlush(new TextWebSocketFrame(
+                "Client " + ctx.channel() + " exit"));
+    }
+}
